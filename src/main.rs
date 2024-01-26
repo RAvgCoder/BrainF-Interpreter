74dc671ca2c53c6995fdc691dd07c0874c9d0f5a
egbor <egborosebhulimen8@gmail.com>
2024-01-26 09:05:43 -0400

    Add performance measurement and optimization flags
    In this commit, I've introduced functionality to measure the execution time of the Brainfuck interpreter with and without optimization. The program now reads a Brainfuck program from a file, parses it, generates the abstract syntax tree, and then runs it using an interpreter. Two modes are implemented: one with optimization enabled and the other with optimization disabled.
    
    Changes Made:
    - Added time measurement functionality to determine the execution time of the interpreter.
    - Implemented a flag to control optimization during parsing.
    - Refactored code for better organization and clarity.
    
    This commit lays the groundwork for optimizing Brainfuck programs and provides a way to measure the impact of optimization on execution time.
    
    Spoiler its slower 😂
    Don't ask why ill profile it and see

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::time::Instant;
+use colored::Colorize;
+use crate::interpreter::Interpreter;
 use crate::parser::Parser;
 
 mod lexer;
 mod grammar;
 mod parser;
+mod interpreter;
 
 struct Program {
     content: String,
@@ -13,12 +16,46 @@ struct Program {
 }
 
 fn main() {
+    // Read the program from file
     let Program { content, should_optimise } = read_file();
-    let mut parser = Parser::new(content, should_optimise);
 
-    let ast = parser.generate_ast();
+    // Init a parser that takes the program and converts it to a token stream
+    let mut parser = Parser::new(content.clone(), should_optimise);
+    let mut parser_unop = Parser::new(content.clone(), false);
 
-    println!("{:#?}", parser);
+
+    time_exec(&mut parser, "Optimised");
+    println!("\n");
+    time_exec(&mut parser_unop, "Un_Optimised");
+}
+
+fn time_exec(parser: &mut Parser, name: &str) {
+    println!("|---------[{}]---------|", name);
+    print!("\t");
+    let start = Instant::now();
+
+    // Generates an abstract syntax tree for the program
+    parser.generate_syntax_tree();
+
+    // Creates the interpreter and runs the code
+    create_interpreter(parser).run_code();
+
+    // Times the run
+    let duration = start.elapsed();
+    println!("	Time taken is: {:?} | Instruction count {:?}", duration, parser.get_num_of_instr());
+    println!("|-------------------------|\n");
+}
+
+fn create_interpreter(parser: &mut Parser) -> Interpreter {
+    match parser.get_ast() {
+        Some(syntax_tree) => {
+            return Interpreter::new(syntax_tree);
+        }
+        None => {
+            eprintln!("{}", "Tree has not been generated yet".red());
+            std::process::exit(255);
+        }
+    }
 }
 
 fn read_file() -> Program {
@@ -33,7 +70,7 @@ fn read_file() -> Program {
     // let optimisation = args.len() > 2;
 
     let file_path = String::from("resources/program.bfk");
-    let optimisation = false;
+    let optimisation = true;
 
     let mut file = match File::open(&file_path) {
         Ok(file) => file,
