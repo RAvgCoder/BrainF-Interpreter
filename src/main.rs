b596e8b5126279a39293f9dc2d054b28e87df054
egbor <egborosebhulimen8@gmail.com>
2024-01-29 09:22:45 -0400

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,57 @@
 use std::fs::File;
 use std::io::Read;
-use std::time::Instant;
+
 use colored::Colorize;
+
 use crate::interpreter::Interpreter;
 use crate::parser::Parser;
 
-mod lexer;
 mod grammar;
-mod parser;
 mod interpreter;
+mod lexer;
+mod parser;
 
+/// Represents a Brainfuck program with its content and optimization flag.
 struct Program {
     content: String,
     should_optimise: bool,
 }
 
+/// Main entry point of the program. Reads a Brainfuck program from a file, parses it,
+/// optimizes it, and executes it using an interpreter.
 fn main() {
     // Read the program from file
-    let Program { content, should_optimise } = read_file();
+    let Program {
+        content,
+        should_optimise,
+    } = read_file();
 
     // Init a parser that takes the program and converts it to a token stream
-    let mut parser = Parser::new(content.clone(), should_optimise);
-    let mut parser_unop = Parser::new(content.clone(), false);
-
-
-    time_exec(&mut parser, "Optimised");
-    println!("\n");
-    time_exec(&mut parser_unop, "Un_Optimised");
-}
-
-fn time_exec(parser: &mut Parser, name: &str) {
-    println!("|---------[{}]---------|", name);
-    print!("\t");
-    let start = Instant::now();
+    let mut parser_optimised = Parser::new(content.clone(), should_optimise);
+    let mut parser_unoptimised = Parser::new(content.clone(), should_optimise);
 
     // Generates an abstract syntax tree for the program
-    parser.generate_syntax_tree();
+    parser_optimised.generate_syntax_tree();
+    parser_unoptimised.generate_syntax_tree();
 
-    // Creates the interpreter and runs the code
-    create_interpreter(parser).run_code();
+    println!(
+        r#"
+| Number of instructions to execute:
+| Optimised: {}
+| Unoptimised: {}
+    "#,
+        parser_optimised.get_num_of_instr(),
+        parser_unoptimised.get_num_of_instr()
+    );
 
-    // Times the run
-    let duration = start.elapsed();
-    println!("	Time taken is: {:?} | Instruction count {:?}", duration, parser.get_num_of_instr());
-    println!("|-------------------------|\n");
+    // Creates the interpreter to run the code
+    let mut interpreter = create_interpreter(&mut parser_optimised);
+
+    // Executes the code
+    interpreter.run_code();
 }
 
+/// Creates an interpreter for the given parser and returns it.
 fn create_interpreter(parser: &mut Parser) -> Interpreter {
     match parser.get_ast() {
         Some(syntax_tree) => {
@@ -58,6 +64,8 @@ fn create_interpreter(parser: &mut Parser) -> Interpreter {
     }
 }
 
+/// Reads a Brainfuck program from a file and returns a `Program` struct containing
+/// the program content and optimization flag.
 fn read_file() -> Program {
     // let args: Vec<String> = env::args().collect();
     //
@@ -74,14 +82,17 @@ fn read_file() -> Program {
 
     let mut file = match File::open(&file_path) {
         Ok(file) => file,
-        Err(why) => panic!("couldn't open {}: {}", file_path, why)
+        Err(why) => panic!("couldn't open {}: {}", file_path, why),
     };
 
     let mut prog = String::new();
     match file.read_to_string(&mut prog) {
         Ok(_) => {}
-        Err(why) => panic!("couldn't read {}: {}", file_path, why)
+        Err(why) => panic!("couldn't read {}: {}", file_path, why),
     }
 
-    Program { content: prog, should_optimise: optimisation }
+    Program {
+        content: prog,
+        should_optimise: optimisation,
+    }
 }
